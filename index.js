9ef0c1767014795fe57b0829670c27d7ce33cb53
Beaumois <louis.beaumois@etudiant.univrennes1.fr>
2024-02-20 17:15:47 +0100

    fonctionnel

---
+++ index.js
@@ -0,0 +1,14 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+function clearDisplay() {
+    display.value = "";
+}
+function calculate() {
+    display.value = eval(display.value);
+}
+function suppr() {
+    display.value = display.value.substring(0, display.value.length - 1);
+}
